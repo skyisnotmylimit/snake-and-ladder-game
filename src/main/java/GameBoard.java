d1aaa3183a6b788d3eb8532686929eb1d5a70b32
Shubham Gupta <shubhamgupta9454666551@gmail.com>
2024-09-02 15:21:15 +0000

    changes

--- src/main/java/GameBoard.java
+++ src/main/java/GameBoard.java
@@ -7,13 +7,13 @@ import gameObjects.Ladders;
 import gameObjects.Player;
 
 public class GameBoard {
-  
+
   private HashSet<Integer> specialBlocks;
   private Snakes snakes;
   private Dice dice;
   private Ladders ladders;
   private List<Player> players;
-  
+
   public GameBoard() {
     this.snakes = new Snakes();
     this.dice = new Dice();
@@ -21,38 +21,36 @@ public class GameBoard {
     this.players = new ArrayList<Player>();
     this.specialBlocks = new HashSet<Integer>();
   }
-  
+
   public void addPlayer(Player player) {
     players.add(player);
   }
 
   public void placeSnake(int head, int tail) {
     specialBlocks.add(head);
-    snakes.addSnake(head,tail);
+    snakes.addSnake(head, tail);
   }
-  
+
   public void placeLadder(int start, int end) {
     specialBlocks.add(start);
-    ladders.addLadder(start,end);
+    ladders.addLadder(start, end);
   }
 
   public void movePlayer(Player player, int steps) {
     int currentPosition = player.getCurrentPosition();
     int newPosition = currentPosition + steps;
-    if(newPosition > 100) {
+    if (newPosition > 100) {
       System.out.println("Player cannot move");
       return;
     }
-    if(specialBlocks.contains(newPosition)) {
-      if(snakes.hasSnake(newPosition)) {
+    if (specialBlocks.contains(newPosition)) {
+      if (snakes.hasSnake(newPosition)) {
         newPosition = snakes.getTail(newPosition);
         System.out.println("Player " + player.getPlayerName() + " has landed on a snake");
-      }
-      else if(ladders.hasLadder(newPosition)) {
+      } else if (ladders.hasLadder(newPosition)) {
         newPosition = ladders.getEnd(newPosition);
         System.out.println("Player " + player.getPlayerName() + " has landed on a ladder");
-      }
-      else {
+      } else {
         System.out.println("Some Error Occured in making the move");
       }
     }
@@ -68,10 +66,9 @@ public class GameBoard {
 
   public boolean isWinner(Player player) {
     int currentPosition = player.getCurrentPosition();
-    if(currentPosition == 100) {
+    if (currentPosition == 100) {
       return true;
-    }
-    else {
+    } else {
       return false;
     }
   }
@@ -79,5 +76,5 @@ public class GameBoard {
   public List<Player> getPlayers() {
     return players;
   }
-  
+
 }
